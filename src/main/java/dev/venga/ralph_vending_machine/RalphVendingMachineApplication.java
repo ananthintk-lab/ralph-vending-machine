0e1d99bef5b4f9763f3ec040532fe31ccd1b915d
ananthintk-lab <ananthintk@gmail.com>
2026-07-01 16:03:32 +0530

    Initial Spring Boot project

---
+++ src/main/java/dev/venga/ralph_vending_machine/RalphVendingMachineApplication.java
@@ -0,0 +1,13 @@
+package dev.venga.ralph_vending_machine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RalphVendingMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RalphVendingMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/venga/ralph_vending_machine/RalphVendingMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.venga.ralph_vending_machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RalphVendingMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
